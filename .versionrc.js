7ed81f099506a4caefd1d37c6d4f9cd143ebcdfa
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-27 08:12:55 -0300

    fix: fix pipeline

---
+++ .versionrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: 'package.json',
+    },
+    {
+      filename: 'ios/bluapp/Info.plist',
+      updater: require.resolve(
+        '@brettdh/standard-version-expo/ios/native/app-version'
+      ),
+    },
+    {
+      filename: 'ios/bluapp/Info.plist',
+      updater: require.resolve(
+        '@brettdh/standard-version-expo/ios/native/buildnum/increment'
+      ),
+    },
+    {
+      filename: 'android/app/build.gradle',
+      updater: require.resolve(
+        '@brettdh/standard-version-expo/android/native/app-version'
+      ),
+    },
+    {
+      filename: 'android/app/build.gradle',
+      updater:
+        require('@brettdh/standard-version-expo/android/native/buildnum/code')(),
+    },
+  ]
+};
