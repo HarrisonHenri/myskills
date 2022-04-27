beafe4ee7e38269a787e3b810c2d08e6603a7212
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-27 08:28:47 -0300

    fix: fix pipeline

--- release.config.js
+++ release.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", {
       "npmPublish": false,
     }],
-    "@semantic-release/github",
-    "@semantic-release/git",
-    {
-     "assets": ["CHANGELOG.md"],
-     "message": "${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}"
-    }
+    "@semantic-release/github"
   ],
 };
