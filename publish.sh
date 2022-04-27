70d9afd36a7d859684abbd1d3831a44eaff52fff
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-27 18:03:52 -0300

    fix: fix code

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yarn standard-version --release-as $1
 git add .
-git commit -m "chore(releas) [ci skip]"
+git commit -m "chore(release) [ci skip]"
 git tag $2
 git push -q https://${GITHUB_TOKEN}@github.com/HarrisonHenri/myskills.git staging --tags
