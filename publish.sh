2ef601b3f797c87378505f921511e7284f09e8c6
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-27 18:28:42 -0300

    feat: add feat

--- publish.sh
+++ publish.sh
@@ -3,5 +3,3 @@ yarn standard-version --release-as $1 --verbose
 git add .
 git commit -m "chore(release): $2 [ci skip]"
 git push -q https://${GITHUB_TOKEN}@github.com/HarrisonHenri/myskills.git staging --tags
-git add CHANGELOG.md
-git commit -m "chore(release): $2 [ci skip]"
