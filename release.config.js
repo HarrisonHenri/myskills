62904d90fdf5753c35aeede03de5902f7d683219
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-27 23:32:57 -0300

    feat: add feat

--- release.config.js
+++ release.config.js
@@ -2,6 +2,7 @@ module.exports = {
   branches: ["staging"],
   plugins: [
     "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     ["@semantic-release/exec", {
       "publishCmd": "./publish.sh ${nextRelease.version} ${nextRelease.gitTag}"
     }],
