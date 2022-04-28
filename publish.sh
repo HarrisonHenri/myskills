298575e4028137fa8573c09121d4de4ee290a421
HarrisonHenri <harrisonhenrisn@gmail.com>
2022-04-28 19:12:42 -0300

    feat: add feat

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yarn standard-version -- --first-release
+yarn standard-version --first-release
 git add .
 git commit -m "chore(release): [ci skip]"
 git push -q https://${GITHUB_TOKEN}@github.com/HarrisonHenri/myskills.git staging --tags
